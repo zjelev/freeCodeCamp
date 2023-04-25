1629a76683a7ba401243bb663f1be18bb43b20c0
zjelev <zlatomir@gmail.com>
2023-04-25 11:25:01 +0300

    Counting Cards

--- jsAlgorithmsAndDS/BasicJS.js
+++ jsAlgorithmsAndDS/BasicJS.js
@@ -1,3 +1,34 @@
+// Counting Cards
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    count++;
+    break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+    count--;
+    break;
+    default:
+      break;
+  }
+
+  if (count <= 0) {
+    return count + " Hold"
+  } else {
+    return count + " Bet"
+  }
+}
+
 // Record Collection 84%
 const recordCollection = {
     2548: {
